1a6cf62336d1d5cdeca1e40fecf3ae1d91ee394f
lazarmatic <lazarmatic91@gmail.com>
2024-10-07 21:55:28 +0200

    lab 2

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,109 @@
+package org.example;
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+//1
+//        String password = "secret";
+//        Scanner reader = new Scanner(System.in);
+//        System.out.println("Please enter your desired password: ");
+//        String userPassword = reader.nextLine();
+//        while (!userPassword.equals(password)){
+//            System.out.println("Enter it again: ");
+//            userPassword = reader.nextLine();
+//        }
+//        if (userPassword.equals(password))
+//            System.out.printf("You guessed right!");
+//2
+//        Scanner reader = new Scanner(System.in);
+//        System.out.println("Please enter a number 1: ");
+//        int num1 = Integer.parseInt(reader.nextLine());
+//        System.out.println("Please enter a number 2: ");
+//        int num2 = Integer.parseInt(reader.nextLine());
+//        System.out.println("Please enter a number 3: ");
+//        int num3 = Integer.parseInt(reader.nextLine());
+//        int suma = num1 + num2 + num3;
+//        System.out.println("Sum of your numbers is: " + (num1 + num2 + num3));
+//3
+//        Scanner reader = new Scanner(System.in);
+//        int suma = 0;
+//        while (true){
+//            System.out.println("Enter a number");
+//            int num1 = Integer.parseInt(reader.nextLine());
+//            suma += num1;
+//            System.out.println(suma);
+//            if (num1 == 0)
+//                break;
+//        }
+//4
+//        Scanner reader = new Scanner(System.in);
+//        System.out.println("Please enter first number: ");
+//        int num1 = Integer.parseInt(reader.nextLine());
+//        System.out.println("Please enter second number: ");
+//        int num2 = Integer.parseInt(reader.nextLine());
+//        while (num1 < num2 - 1){
+//            num1++;
+//            System.out.println(num1);
+//            }
+//        if (num1 > num2){
+//            System.out.println(num1 + " " + num2);
+//        }
+//5
+//        int number = 2;
+//        int powerNumber = 4;
+//            for (int j = 0; j < powerNumber + 1; j++) {
+//                System.out.println((int)Math.pow(number, j));
+//            }
+//6
+//        Scanner reader = new Scanner(System.in);
+//        System.out.println("Please enter a number: ");
+//        int number = Integer.parseInt(reader.nextLine());
+//        for (int i = 0; i < number; i++){
+//            System.out.println("In the beginning there were the swamp, the hoe and Java");
+//        }
+//7
+//        for (int i = 0; i<10; i++){
+//            for (int j = i; j < 10; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println("");
+//        }
+//8
+//        drawStarsPiramid(5);
+//        public static void drawStarsPiramid(int rows){
+//            for (int i = 1; i<rows+1; i++){
+//                for (int j = 0; j < i; j++){
+//                    System.out.print("*");
+//                }
+//                System.out.println("");
+//            }
+//9
+//        drawNumbersPiramid(5);
+//        public static void drawNumbersPiramid(int rows){
+//            for (int i = 1; i<rows+2; i++){
+//                for (int j = 1; j<i; j++){
+//                    System.out.print(j);
+//                }
+//                System.out.println("");
+//            }
+//    }
+//10
+//        Scanner reader = new Scanner(System.in);
+//        int number = 75;
+//        int counter = 0;
+//        System.out.println("Guess the number: ");
+//        while (true) {
+//            int guess = Integer.parseInt(reader.nextLine());
+//            counter++;
+//            if (guess > number)
+//                System.out.println("Lower! - this is your " + counter + " guess");
+//            else if (guess < number)
+//                System.out.println("Higher! - this is your " + counter + " guess");
+//            else {System.out.println("You are correct!");
+//                System.out.println("You made " + counter + " guesses");
+//                break;}
+//                  }
+    }
+}
+
